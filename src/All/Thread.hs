9134f3a15f8292dfe109067616dcd7c31048c2b6
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-21 17:32:51 -0400

    consolidating things into single files.. to make life easier for a while

--- src/All/Thread.hs
+++ src/All/Thread.hs
@@ -1,6 +1,28 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Model.Thread.Internal (
+module Handler.Thread (
+  getThreadsR,
+  postThreadR0,
+  getThreadR,
+  getThreadH,
+  putThreadR,
+  deleteThreadR,
+
+  getCountThreadsR,
+
+  getThreadStatsR,
+  getThreadStatR,
+
+  getThreadPacksR,
+  getThreadPackR,
+  getThreadPackH,
+
+  -- Model/Function
+  threadRequestToThread,
+  threadToResponse,
+  threadsToResponses,
+
+  -- Model/Internal
   getThreadsM,
 
   getThreadM,
@@ -23,12 +45,183 @@ module Model.Thread.Internal (
 
 
 
+import           Handler.Prelude
+import           Model.Thread
 import           Database.Esqueleto     ((^.))
 import qualified Database.Esqueleto     as E
 import           Model.Prelude
 import           Model.Thread.Function
 
 
+
+
+getThreadsR :: Handler Value
+getThreadsR = do
+
+  user_id <- requireAuthId
+
+  (toJSON . threadsToResponses) <$> getThreadsM user_id
+
+
+
+postThreadR0 :: Handler Value
+postThreadR0 = do
+
+  user_id <- requireAuthId
+
+  sp <- lookupStandardParams
+
+  case (spBoardId sp) of
+    Nothing -> notFound
+    (Just board_id) -> do
+
+      thread_request <- requireJsonBody :: Handler ThreadRequest
+      (toJSON . threadToResponse) <$> insertThreadM user_id board_id thread_request
+
+
+
+getThreadR :: ThreadId -> Handler Value
+getThreadR thread_id = do
+  user_id <- requireAuthId
+  (toJSON . threadToResponse) <$> getThreadM user_id thread_id
+
+
+
+getThreadH :: Text -> Handler Value
+getThreadH thread_name = do
+  user_id <- requireAuthId
+  (toJSON . threadToResponse) <$> getThreadMH user_id thread_name
+
+
+
+putThreadR :: ThreadId -> Handler Value
+putThreadR thread_id = do
+  user_id <- requireAuthId
+  thread_request <- requireJsonBody
+  (toJSON . threadToResponse) <$> updateThreadM user_id thread_id thread_request
+
+
+
+deleteThreadR :: ThreadId -> Handler Value
+deleteThreadR thread_id = do
+  user_id <- requireAuthId
+  void $ deleteThreadM user_id thread_id
+  pure $ toJSON ()
+
+
+
+getCountThreadsR :: Handler Value
+getCountThreadsR = do
+  user_id <- requireAuthId
+  toJSON <$> countThreadsM user_id
+
+
+
+getThreadStatsR :: Handler Value
+getThreadStatsR = do
+  user_id <- requireAuthId
+  toJSON <$> getThreadStatsM user_id
+
+
+
+getThreadStatR :: ThreadId -> Handler Value
+getThreadStatR thread_id = do
+  user_id <- requireAuthId
+  toJSON <$> getThreadStatM user_id thread_id
+
+
+
+getThreadPacksR :: Handler Value
+getThreadPacksR = do
+  user_id <- requireAuthId
+  toJSON <$> getThreadPacksM user_id
+
+
+
+getThreadPackR :: ThreadId -> Handler Value
+getThreadPackR thread_id = do
+  user_id <- requireAuthId
+  toJSON <$> getThreadPackM user_id thread_id
+
+
+
+getThreadPackH :: Text -> Handler Value
+getThreadPackH thread_name = do
+  user_id <- requireAuthId
+  toJSON <$> getThreadPackMH user_id thread_name
+
+
+
+
+
+
+
+
+--
+-- Model/Function
+--
+
+threadRequestToThread :: UserId -> BoardId -> ThreadRequest -> Thread
+threadRequestToThread user_id board_id ThreadRequest{..} = Thread {
+  threadUserId      = user_id,
+  threadBoardId     = board_id,
+  threadName        = toPrettyUrl threadRequestDisplayName,
+  threadDisplayName = threadRequestDisplayName,
+  threadDescription = threadRequestDescription,
+  threadSticky      = threadRequestSticky,
+  threadLocked      = threadRequestLocked,
+  threadPoll        = threadRequestPoll,
+  threadIcon        = threadRequestIcon,
+  threadTags        = threadRequestTags,
+  threadActive      = True,
+  threadGuard       = threadRequestGuard,
+  threadCreatedAt   = Nothing,
+  threadModifiedBy  = Nothing,
+  threadModifiedAt  = Nothing,
+  threadActivityAt  = Nothing
+}
+
+
+
+threadToResponse :: Entity Thread -> ThreadResponse
+threadToResponse (Entity thread_id Thread{..}) = ThreadResponse {
+  threadResponseId          = keyToInt64 thread_id,
+  threadResponseUserId      = keyToInt64 threadUserId,
+  threadResponseBoardId     = keyToInt64 threadBoardId,
+  threadResponseName        = threadName,
+  threadResponseDisplayName = threadDisplayName,
+  threadResponseDescription = threadDescription,
+  threadResponseSticky      = threadSticky,
+  threadResponseLocked      = threadLocked,
+  threadResponsePoll        = threadPoll,
+  threadResponseIcon        = threadIcon,
+  threadResponseTags        = threadTags,
+  threadResponseActive      = threadActive,
+  threadResponseGuard       = threadGuard,
+  threadResponseCreatedAt   = threadCreatedAt,
+  threadResponseModifiedBy  = fmap keyToInt64 threadModifiedBy,
+  threadResponseModifiedAt  = threadModifiedAt,
+  threadResponseActivityAt  = threadActivityAt
+}
+
+
+
+threadsToResponses :: [Entity Thread] -> ThreadResponses
+threadsToResponses threads = ThreadResponses {
+  threadResponses = map threadToResponse threads
+}
+
+
+
+
+
+
+
+
+
+--
+-- Model/Internal
+--
 
 -- orderByToField :: forall typ record. OrderBy -> EntityField record typ
 -- TODO FIXME, this type sig might cause problems
--- src/Model/Thread.hs
+++
@@ -1,9 +0,0 @@
-module Model.Thread (
-  module A
-) where
-
-
-
-import           Model.Thread.Function as A
-import           Model.Thread.Internal  as A
-import           Model.Pack.Thread      as A
