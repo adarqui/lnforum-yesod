5c702405493d027aeb89d789c7ebe947f1521eec
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-21 17:20:34 -0400

    moving all derive persist calls to one file

--- src/Model/DerivePersist.hs
+++ src/Model/DerivePersist.hs
@@ -1,17 +1,29 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module Model.DerivePersist (
+  module A,
   Membership,
-  Visibility
+  Visibility,
+  LikeOpt,
+  ProfileGender
 ) where
 
 
 
 import           Database.Persist.TH
+import           LN.T.Ent            as A
+import           LN.T.LeuronTraining as A
+import           LN.T.Like
 import           LN.T.Membership
+import           LN.T.Profile        as A
 import           LN.T.Visibility
+
 
 
 
 derivePersistField "Membership"
 derivePersistField "Visibility"
+derivePersistField "Ent"
+derivePersistField "LeuronTrainingSummary"
+derivePersistField "LikeOpt"
+derivePersistField "ProfileGender"
