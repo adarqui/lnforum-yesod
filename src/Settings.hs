da54aae6539b681749618615eeb5843c96c8a859
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-28 17:40:48 -0400

    cleanup

--- src/Settings.hs
+++ src/Settings.hs
@@ -7,7 +7,7 @@
 -- In addition, you can configure a number of different aspects of Yesod
 -- by overriding methods in the Yesod typeclass. That instance is
 -- declared in the Foundation.hs file.
---
+
 module Settings (
   DB,
   OAuthKeys (..),
