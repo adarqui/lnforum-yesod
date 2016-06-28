8a72863e65584c947ab039c8d9908ecf6c269825
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-28 15:09:46 -0400

    restructuring standard params, api errors, etc

--- src/All/Pack/PmIn.hs
+++ src/All/Pack/PmIn.hs
@@ -25,14 +25,15 @@ import           All.User
 getPmInPacksR :: Handler Value
 getPmInPacksR = run $ do
   user_id <- _requireAuthId
-  toJSON <$> getPmInPacksM user_id
+  sp      <- lookupStandardParams
+  errorOrJSON id $ getPmInPacksM (pure sp) user_id
 
 
 
 getPmInPackR :: PmInId -> Handler Value
 getPmInPackR thread_post_id = run $ do
   user_id <- _requireAuthId
-  toJSON <$> getPmInPackM user_id thread_post_id
+  errorOrJSON id $ getPmInPackM user_id thread_post_id
 
 
 
@@ -43,38 +44,36 @@ getPmInPackR thread_post_id = run $ do
 -- Model
 --
 
-getPmInPacksM :: UserId -> HandlerEff PmInPackResponses
-getPmInPacksM user_id = do
+getPmInPacksM :: Maybe StandardParams -> UserId -> HandlerErrorEff PmInPackResponses
+getPmInPacksM m_sp user_id = do
 
-  sp <- lookupStandardParams
+  e_pm_ins <- getPmInsM m_sp user_id
 
-  pmIns <- getPmInsM user_id
-
-  pm_in_packs <- mapM (\pmIn -> getPmInPack_ByPmInM user_id pmIn sp) pmIns
-
-  return $ PmInPackResponses {
-    pmInPackResponses = pm_in_packs
-  }
+  rehtie e_pm_ins left $ \pm_ins -> do
+    pm_in_packs <- rights <$> mapM (\pm_in -> getPmInPack_ByPmInM user_id pm_in) pm_ins
+    right $ PmInPackResponses {
+      pmInPackResponses = pm_in_packs
+    }
 
 
 
-getPmInPackM :: UserId -> PmInId -> HandlerEff PmInPackResponse
+getPmInPackM :: UserId -> PmInId -> HandlerErrorEff PmInPackResponse
 getPmInPackM user_id pm_in_id = do
 
-  pmIn <- getPmInM user_id pm_in_id
-
-  getPmInPack_ByPmInM user_id pmIn defaultStandardParams
-
+  e_pm_in <- getPmInM user_id pm_in_id
+  rehtie e_pm_in left $ getPmInPack_ByPmInM user_id
 
 
-getPmInPack_ByPmInM :: UserId -> Entity PmIn -> StandardParams -> HandlerEff PmInPackResponse
-getPmInPack_ByPmInM user_id pmIn@(Entity pm_in_id PmIn{..}) _ = do
 
-  pm_in_user <- getUserM user_id pmInUserId
+getPmInPack_ByPmInM :: UserId -> Entity PmIn -> HandlerErrorEff PmInPackResponse
+getPmInPack_ByPmInM user_id pmIn@(Entity pm_in_id PmIn{..}) = do
 
-  return $ PmInPackResponse {
-    pmInPackResponsePmIn    = pmInToResponse pmIn,
-    pmInPackResponsePmInId  = keyToInt64 pm_in_id,
-    pmInPackResponseUser    = userToSanitizedResponse pm_in_user,
-    pmInPackResponseUserId  = entityKeyToInt64 pm_in_user
-  }
+  e_pm_in_user <- getUserM user_id pmInUserId
+
+  rehtie e_pm_in_user left $ \pm_in_user -> do
+    right $ PmInPackResponse {
+      pmInPackResponsePmIn    = pmInToResponse pmIn,
+      pmInPackResponsePmInId  = keyToInt64 pm_in_id,
+      pmInPackResponseUser    = userToSanitizedResponse pm_in_user,
+      pmInPackResponseUserId  = entityKeyToInt64 pm_in_user
+    }
