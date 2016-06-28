4464421cfc4e6ea6838f8144c2c3c75660ac43fa
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-28 17:30:44 -0400

    cleanup

--- src/Helper/Request.hs
+++ src/Helper/Request.hs
@@ -14,27 +14,27 @@ import           Import
 
 
 lookupUtf8Header :: HeaderName -> HandlerEff (Maybe Text)
-lookupUtf8Header headerName = return . fmap decodeUtf8 =<< lookupHeader headerName
+lookupUtf8Header headerName = pure . fmap decodeUtf8 =<< lookupHeader headerName
 
 
 
 allowCrossOrigin :: HandlerEff ()
 allowCrossOrigin = do
-    mo <- lookupUtf8Header "Origin"
-    mrh <- lookupUtf8Header "Access-Control-Req-Headers"
+  mo <- lookupUtf8Header "Origin"
+  mrh <- lookupUtf8Header "Access-Control-Req-Headers"
 
-    case mo of
-        Just o  -> addHeader "Access-Control-Allow-Origin" o
-        Nothing -> return ()
+  case mo of
+    Just o  -> addHeader "Access-Control-Allow-Origin" o
+    Nothing -> pure ()
 
-    case mrh of
-        Just rh -> addHeader "Access-Control-Allow-Headers" rh
-        Nothing -> return ()
+  case mrh of
+    Just rh -> addHeader "Access-Control-Allow-Headers" rh
+    Nothing -> pure ()
 
-    addHeader "Access-Control-Allow-Methods" "POST, GET, OPTIONS"
-    addHeader "Access-Control-Allow-Credentials" "true"
+  addHeader "Access-Control-Allow-Methods" "POST, GET, OPTIONS"
+  addHeader "Access-Control-Allow-Credentials" "true"
 
 
 
 fromMaybe404 :: HandlerEff (Maybe a) -> HandlerEff a
-fromMaybe404 f = maybe notFound return =<< f
+fromMaybe404 f = maybe notFound pure =<< f
