a8321d3d1712c16aa474b3c35820044206314959
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-28 14:14:23 -0400

    restructuring standard params, api errors, etc

--- src/All/Team.hs
+++ src/All/Team.hs
@@ -43,47 +43,23 @@ import           LN.T.Visibility
 getTeamsR :: Handler Value
 getTeamsR = run $ do
   user_id <- _requireAuthId
-  (toJSON . teamsToResponses) <$> getTeamsM user_id
-
-
-
--- INFO: Can't insert teams
---
--- postTeamR0 :: Handler Value
--- postTeamR0 = run $ do
---   user_id <- _requireAuthId
---   sp <- lookupStandardParams
---   case (spOrganizationId sp) of
---     Nothing -> notFound
---     Just org_id -> do
---       team_request <- requireJsonBody :: HandlerEff TeamRequest
---       (toJSON . teamToResponse) <$> insertTeamM user_id org_id team_request
+  sp      <- lookupStandardParams
+  errorOrJSON teamsToResponses $ getTeamsM (pure sp) user_id
 
 
 
 getTeamR :: TeamId -> Handler Value
 getTeamR team_id = run $ do
   user_id <- _requireAuthId
-  (toJSON . teamToResponse) <$> getTeamM user_id team_id
+  errorOrJSON teamToResponse $ getTeamM user_id team_id
 
 
 
 putTeamR :: TeamId -> Handler Value
 putTeamR team_id = run $ do
-  user_id <- _requireAuthId
+  user_id      <- _requireAuthId
   team_request <- requireJsonBody
-  (toJSON . teamToResponse) <$> updateTeamM user_id team_id team_request
-
-
-
--- INFO: Can't delete teams
---
--- deleteTeamR :: TeamId -> Handler Value
--- deleteTeamR team_id = run $ do
---   user_id <- _requireAuthId
---   void $ deleteTeamM user_id team_id
---   pure $ toJSON ()
-
+  errorOrJSON teamToResponse $ updateTeamM user_id team_id team_request
 
 
 
@@ -141,13 +117,6 @@ teamsToResponses teams = TeamResponses {
 
 
 
--- textToSystemTeam :: Text -> Maybe SystemTeam
--- textToSystemTeam name =
---   case name of
---     owners -> Just Team_
---   where
---   owners = show Team_Owners
---   members = show Team_Members
 
 
 
@@ -159,60 +128,47 @@ teamsToResponses teams = TeamResponses {
 -- Model/Internal
 --
 
-getTeamsM :: UserId -> HandlerEff [Entity Team]
-getTeamsM user_id = do
+getTeamsM :: Maybe StandardParams -> UserId -> HandlerErrorEff [Entity Team]
+getTeamsM m_sp user_id = do
 
-  sp@StandardParams{..} <- lookupStandardParams
-
-  case (spOrganizationId, spUserId) of
-    (Just org_id, Nothing)         -> getTeams_ByOrganizationIdM user_id org_id sp
-    (Nothing, Just lookup_user_id) -> getTeams_ByUserIdM user_id lookup_user_id sp
-    (_, _)                         -> notFound
+  case (lookupSpMay m_sp spOrganizationId, lookupSpMay m_sp spUserId) of
+    (Just org_id, Nothing)         -> getTeams_ByOrganizationIdM m_sp user_id org_id
+    (Nothing, Just lookup_user_id) -> getTeams_ByUserIdM m_sp user_id lookup_user_id
+    _                              -> left $ Error_InvalidArguments "org_id, user_id"
 
 
 
-getTeams_ByOrganizationIdM :: UserId -> OrganizationId -> StandardParams -> HandlerEff [Entity Team]
-getTeams_ByOrganizationIdM _ org_id sp = do
-  selectListDb sp [TeamOrgId ==. org_id] [] TeamId
+getTeams_ByOrganizationIdM :: Maybe StandardParams -> UserId -> OrganizationId -> HandlerErrorEff [Entity Team]
+getTeams_ByOrganizationIdM m_sp _ org_id = do
+  selectListDbEither m_sp [TeamOrgId ==. org_id, TeamActive ==. True] [] TeamId
 
 
 
-getTeams_ByUserIdM :: UserId -> UserId -> StandardParams -> HandlerEff [Entity Team]
-getTeams_ByUserIdM _ lookup_user_id sp = do
-  selectListDb sp [TeamUserId ==. lookup_user_id] [] TeamId
+getTeams_ByUserIdM :: Maybe StandardParams -> UserId -> UserId -> HandlerErrorEff [Entity Team]
+getTeams_ByUserIdM m_sp _ lookup_user_id = do
+  selectListDbEither m_sp [TeamUserId ==. lookup_user_id, TeamActive ==. True] [] TeamId
 
 
 
-getTeamM :: UserId -> TeamId -> HandlerEff (Entity Team)
+getTeamM :: UserId -> TeamId -> HandlerErrorEff (Entity Team)
 getTeamM _ team_id = do
-  notFoundMaybe =<< selectFirstDb [ TeamId ==. team_id ] []
+  selectFirstDbEither [TeamId ==. team_id] []
 
 
 
-getTeamMH :: UserId -> Text -> OrganizationId -> HandlerEff (Entity Team)
+getTeamMH :: UserId -> Text -> OrganizationId -> HandlerErrorEff (Entity Team)
 getTeamMH _ team_sid org_id = do
   case m_system_team of
     Nothing          -> notFound
     Just system_team -> do
-      notFoundMaybe =<< selectFirstDb [ TeamOrgId ==. org_id, TeamSystem ==. system_team ] []
+      selectFirstDbEither [TeamOrgId ==. org_id, TeamSystem ==. system_team, TeamActive ==. True] []
 
   where
   m_system_team = readMay $ T.unpack team_sid
 
 
 
-
--- insertTeamM :: UserId -> OrganizationId -> TeamRequest -> HandlerEff (Entity Team)
--- insertTeamM user_id org_id team_request = do
--- --  sp <- lookupStandardParams
---   ts <- timestampH'
---   let
---     team = (teamRequestToTeam user_id org_id team_request) { teamCreatedAt = Just ts }
---   insertEntityDb team
-
-
-
-insertTeam_InternalM :: UserId -> OrganizationId -> SystemTeam -> TeamRequest -> HandlerEff (Entity Team)
+insertTeam_InternalM :: UserId -> OrganizationId -> SystemTeam -> TeamRequest -> HandlerErrorEff (Entity Team)
 insertTeam_InternalM user_id org_id system_team team_request = do
 
   ts <- timestampH'
@@ -220,27 +176,30 @@ insertTeam_InternalM user_id org_id system_team team_request = do
   let
     team = (teamRequestToTeam user_id org_id team_request) { teamCreatedAt = Just ts }
 
-  insertEntityDb $ team { teamSystem = system_team }
+  insertEntityDbEither $ team { teamSystem = system_team }
 
 
 
-insert_SystemTeamsM :: UserId -> OrganizationId -> HandlerEff ()
+insert_SystemTeamsM :: UserId -> OrganizationId -> HandlerErrorEff ()
 insert_SystemTeamsM user_id org_id = do
 
-  -- bg job: Insert owners team
-  (Entity owners_id team) <- insertTeam_InternalM user_id org_id Team_Owners (TeamRequest Membership_InviteOnly Nothing [] Public 0)
-  void $ insertTeamMember_BypassM user_id org_id owners_id (TeamMemberRequest 0)
+  void $ runEitherT $ do
+    -- bg job: Insert owners team
+    (Entity owners_id team) <- isT $ insertTeam_InternalM user_id org_id Team_Owners (TeamRequest Membership_InviteOnly Nothing [] Public 0)
+    void $ isT $ insertTeamMember_BypassM user_id org_id owners_id (TeamMemberRequest 0)
 
-  -- bg job: Insert members team
-  (Entity members_id team) <- insertTeam_InternalM user_id org_id Team_Members (TeamRequest Membership_Join Nothing [] Public 0)
-  void $ insertTeamMember_BypassM user_id org_id members_id (TeamMemberRequest 0)
+    -- bg job: Insert members team
+    (Entity members_id team) <- isT $ insertTeam_InternalM user_id org_id Team_Members (TeamRequest Membership_Join Nothing [] Public 0)
+    void $ isT $ insertTeamMember_BypassM user_id org_id members_id (TeamMemberRequest 0)
 
-  return ()
+    pure ()
+
+  right ()
 
 
 
 
-updateTeamM :: UserId -> TeamId -> TeamRequest -> HandlerEff (Entity Team)
+updateTeamM :: UserId -> TeamId -> TeamRequest -> HandlerErrorEff (Entity Team)
 updateTeamM user_id team_id team_request = do
 
   ts <- timestampH'
@@ -249,7 +208,7 @@ updateTeamM user_id team_id team_request = do
     Team{..} = (teamRequestToTeam user_id dummyId team_request) { teamModifiedAt = Just ts }
 
   updateWhereDb
-    [ TeamUserId ==. user_id, TeamId ==. team_id ]
+    [TeamUserId ==. user_id, TeamId ==. team_id, TeamActive ==. True]
     [ TeamModifiedAt  =. teamModifiedAt
     , TeamMembership  =. teamMembership
     , TeamIcon        =. teamIcon
@@ -258,25 +217,23 @@ updateTeamM user_id team_id team_request = do
     , TeamGuard      +=. teamGuard
     ]
 
-  notFoundMaybe =<< selectFirstDb [ TeamUserId ==. user_id, TeamId ==. team_id ] []
+  selectFirstDbEither [TeamUserId ==. user_id, TeamId ==. team_id, TeamActive ==. True] []
 
 
 
-deleteTeamM :: UserId -> TeamId -> HandlerEff ()
+deleteTeamM :: UserId -> TeamId -> HandlerErrorEff ()
 deleteTeamM user_id team_id = do
-  deleteWhereDb [ TeamUserId ==. user_id, TeamId ==. team_id ]
+  deleteWhereDbEither [TeamUserId ==. user_id, TeamId ==. team_id, TeamActive ==. True]
 
 
 
-getTeamCountM :: HandlerEff Int
-getTeamCountM = do
-  return 0
---  _runDB $ count [ TeamName !=. "" ]
+getTeamCountM :: HandlerErrorEff Int
+getTeamCountM = right 2
 
 
 
-getTeamStatM :: UserId -> TeamId -> HandlerEff TeamStatResponse
+getTeamStatM :: UserId -> TeamId -> HandlerErrorEff TeamStatResponse
 getTeamStatM _ team_id = do
-  return $ TeamStatResponse {
+  right $ TeamStatResponse {
     teamStatResponseMembers = 0 -- TODO FIXME
   }
