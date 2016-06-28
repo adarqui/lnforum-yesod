583a9408324be8ad50f9d777d5b8230d9010b088
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-28 04:14:27 -0400

    restructuring standard params, api errors, etc

--- src/All/Pack/Leuron.hs
+++ src/All/Pack/Leuron.hs
@@ -26,14 +26,15 @@ import           All.User
 getLeuronPacksR :: Handler Value
 getLeuronPacksR = run $ do
   user_id <- _requireAuthId
-  toJSON <$> getLeuronPacksM user_id
+  sp      <- lookupStandardParams
+  errorOrJSON id $ getLeuronPacksM (pure sp) user_id
 
 
 
 getLeuronPackR :: LeuronId -> Handler Value
 getLeuronPackR leuron_id = run $ do
   user_id <- _requireAuthId
-  toJSON <$> getLeuronPackM user_id leuron_id
+  errorOrJSON id $ getLeuronPackM user_id leuron_id
 
 
 
@@ -45,53 +46,55 @@ getLeuronPackR leuron_id = run $ do
 -- Model
 --
 
-getLeuronPacksM :: UserId -> HandlerEff LeuronPackResponses
-getLeuronPacksM user_id = do
+getLeuronPacksM :: Maybe StandardParams -> UserId -> HandlerErrorEff LeuronPackResponses
+getLeuronPacksM m_sp user_id = do
 
-  sp <- lookupStandardParams
+  e_leurons <- getLeuronsM user_id
+  rehtie e_leurons left $ \leurons -> do
 
-  leurons <- getLeuronsM user_id
+    leuron_packs <- fmap rights $ mapM (\leuron -> getLeuronPack_ByLeuronM user_id leuron) leurons
 
-  leuron_packs <- mapM (\leuron -> getLeuronPack_ByLeuronM user_id leuron sp) leurons
-
-
-  return $ LeuronPackResponses {
-    leuronPackResponses = leuron_packs
-  }
+    right $ LeuronPackResponses {
+      leuronPackResponses = leuron_packs
+    }
 
 
 
-getLeuronPackM :: UserId -> LeuronId -> HandlerEff LeuronPackResponse
+getLeuronPackM :: UserId -> LeuronId -> HandlerErrorEff LeuronPackResponse
 getLeuronPackM user_id leuron_id = do
 
-  leuron <- getLeuronM user_id leuron_id
-
-  getLeuronPack_ByLeuronM user_id leuron defaultStandardParams
-
+  e_leuron <- getLeuronM user_id leuron_id
+  rehtie e_leuron left $ \leuron -> getLeuronPack_ByLeuronM user_id leuron
 
 
-getLeuronPack_ByLeuronM :: UserId -> Entity Leuron -> StandardParams -> HandlerEff LeuronPackResponse
-getLeuronPack_ByLeuronM user_id leuron@(Entity leuron_id Leuron{..}) _ = do
 
-  leuron_user     <- getUserM user_id leuronUserId
-  leuron_stat     <- getLeuronStatM user_id leuron_id
---  leuron_like <- getLeuronLike_ByLeuronM user_id leuron
---  leuron_star <- getLeuronStar_ByLeuronM user_id leuron
-  leuron_training <- insertLeuronTrainingM user_id leuron_id $ LeuronTrainingRequest LTS_View 0
+getLeuronPack_ByLeuronM :: UserId -> Entity Leuron -> HandlerErrorEff LeuronPackResponse
+getLeuronPack_ByLeuronM user_id leuron@(Entity leuron_id Leuron{..}) = do
 
-  return $ LeuronPackResponse {
-    leuronPackResponseLeuron      = leuronToResponse leuron,
-    leuronPackResponseLeuronId    = keyToInt64 leuron_id,
-    leuronPackResponseUser        = userToSanitizedResponse leuron_user,
-    leuronPackResponseUserId      = entityKeyToInt64 leuron_user,
-    -- TODO FIXME
-    leuronPackResponseTraining    = leuronTrainingToResponse leuron_training,
-    -- TODO FIXME
-    -- leuronPackResponseTrainingId
-    leuronPackResponseStat        = leuron_stat,
-    leuronPackResponseLike        = Nothing,
-    leuronPackResponseStar        = Nothing,
-    leuronPackResponsePermissions = emptyPermissions
---    leuronPackResponseLike     = fmap leuronLikeToResponse leuron_like,
---    leuronPackResponseStar     = fmap leuronStarToResponse leuron_star
-  }
+  lr <- runEitherT $ do
+    leuron_user     <- isT $ getUserM user_id leuronUserId
+    leuron_stat     <- isT $ getLeuronStatM user_id leuron_id
+    leuron_training <- isT $ insertLeuronTrainingM user_id leuron_id $ LeuronTrainingRequest LTS_View 0
+
+    pure (leuron_user
+         ,leuron_stat
+         ,leuron_training)
+
+  rehtie lr left $ \(leuron_user, leuron_stat, leuron_training) -> do
+
+    right $ LeuronPackResponse {
+      leuronPackResponseLeuron      = leuronToResponse leuron,
+      leuronPackResponseLeuronId    = keyToInt64 leuron_id,
+      leuronPackResponseUser        = userToSanitizedResponse leuron_user,
+      leuronPackResponseUserId      = entityKeyToInt64 leuron_user,
+      -- TODO FIXME
+      leuronPackResponseTraining    = leuronTrainingToResponse leuron_training,
+      -- TODO FIXME
+      -- leuronPackResponseTrainingId
+      leuronPackResponseStat        = leuron_stat,
+      leuronPackResponseLike        = Nothing,
+      leuronPackResponseStar        = Nothing,
+      leuronPackResponsePermissions = emptyPermissions
+  --    leuronPackResponseLike     = fmap leuronLikeToResponse leuron_like,
+  --    leuronPackResponseStar     = fmap leuronStarToResponse leuron_star
+    }
