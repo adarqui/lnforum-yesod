f57de3f97310f0b75a047f57f2ecbdc490a13c15
Andrew Darqui <andrew.darqui@gmail.com>
2016-08-09 17:31:14 -0400

    mk jobs

--- src/LN/Job/Enqueue.hs
+++ src/LN/Job/Enqueue.hs
@@ -3,7 +3,9 @@
 
 module LN.Job.Enqueue (
   mkJob_CreateUserProfile,
-  mkJob_CreateUserApi
+  mkJob_CreateUserApi,
+  mkJob_AddThreadPostToSet,
+  mkJob_RemoveThreadPostFromSet
 ) where
 
 
@@ -28,3 +30,15 @@ mkJob_CreateUserApi :: UserId -> ApiRequest -> IO ()
 mkJob_CreateUserApi user_id api_request = do
   bgRunEnq QCreateUserApi $ bgEnq (user_id, api_request)
   pure ()
+
+
+
+mkJob_AddThreadPostToSet :: ThreadId -> ThreadPostId -> IO ()
+mkJob_AddThreadPostToSet thread_id post_id = do
+  bgRunEnq QAddThreadPostToSet $ bgEnq (thread_id, post_id)
+
+
+
+mkJob_RemoveThreadPostFromSet :: ThreadId -> ThreadPostId -> IO ()
+mkJob_RemoveThreadPostFromSet thread_id post_id = do
+  bgRunEnq QRemoveThreadPostFromSet $ bgEnq (thread_id, post_id)
